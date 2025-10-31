e7aceff62cfe64219e784450e01d9b48c1c19767
Jose Miguel Cruz Cortes <189800064+tlato423@users.noreply.github.com>
2025-10-30 21:07:52 -0600

    Add files via upload

---
+++ build_in.py
@@ -0,0 +1,21 @@
+numeros = [4, 7, 1, 42, 15]
+
+#encontrando el numero mas alto de una lista
+numero_mas_alto = max(numeros)
+
+#encontrando el numero mas pequeño en una lista
+numero_mas_bajo = min(numeros)
+
+#redondeando a 6 decimales
+numero = round(12.345678,4)
+
+#retorna False -> 0, vacio, False, none
+resultado_bool = bool()
+
+#retorna true, si todos los valores son verdaderos
+resultado_all = all([2, "true",[344,23]])
+
+#suma todos los valores de un iterable
+suma_total = sum(numeros)
+
+print(suma_total)
